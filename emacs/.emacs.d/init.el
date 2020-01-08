2bf25426fb558a7dfc75cc610de61fbe6ce5fa01
John Michael Wu <johnmichaelwu@gmail.com>
2020-01-08 20:09:31 +0800

    Minor improvements.

--- emacs/.emacs.d/init.el
+++ emacs/.emacs.d/init.el
@@ -1,2 +1,17 @@
 (setq vc-follow-symlinks nil)
 (org-babel-load-file "~/.emacs.d/config.org")
+(custom-set-variables
+ ;; custom-set-variables was added by Custom.
+ ;; If you edit it by hand, you could mess it up, so be careful.
+ ;; Your init file should contain only one such instance.
+ ;; If there is more than one, they won't work right.
+ '(custom-enabled-themes (quote (manoj-dark)))
+ '(package-selected-packages
+   (quote
+    (pyim cdlatex htmlize emms elpy lorem-ipsum bash-completion ace-window ein w3m google-this magit utop tuareg haskell-mode julia-mode julia-repl buffer-move python-mode sage-shell-mode auctex md4rd racket-mode markdown-mode proof-general))))
+(custom-set-faces
+ ;; custom-set-faces was added by Custom.
+ ;; If you edit it by hand, you could mess it up, so be careful.
+ ;; Your init file should contain only one such instance.
+ ;; If there is more than one, they won't work right.
+ )
--- texmf/texmf/tex/latex/jmw/auto/jmwcommon.el
+++ texmf/texmf/tex/latex/jmw/auto/jmwcommon.el
@@ -3,12 +3,12 @@
  (lambda ()
    (TeX-add-to-alist 'LaTeX-provided-package-options
                      '(("hyperref" "colorlinks=true") ("mdframed" "framemethod=TikZ")))
-   (add-to-list 'LaTeX-verbatim-macros-with-braces-local "path")
-   (add-to-list 'LaTeX-verbatim-macros-with-braces-local "url")
-   (add-to-list 'LaTeX-verbatim-macros-with-braces-local "nolinkurl")
-   (add-to-list 'LaTeX-verbatim-macros-with-braces-local "hyperbaseurl")
-   (add-to-list 'LaTeX-verbatim-macros-with-braces-local "hyperimage")
    (add-to-list 'LaTeX-verbatim-macros-with-braces-local "hyperref")
+   (add-to-list 'LaTeX-verbatim-macros-with-braces-local "hyperimage")
+   (add-to-list 'LaTeX-verbatim-macros-with-braces-local "hyperbaseurl")
+   (add-to-list 'LaTeX-verbatim-macros-with-braces-local "nolinkurl")
+   (add-to-list 'LaTeX-verbatim-macros-with-braces-local "url")
+   (add-to-list 'LaTeX-verbatim-macros-with-braces-local "path")
    (add-to-list 'LaTeX-verbatim-macros-with-delims-local "path")
    (TeX-run-style-hooks
     "amsmath"
@@ -17,6 +17,7 @@
     "xcolor"
     "hyperref"
     "graphicx"
+    "siunitx"
     "amsthm"
     "thmtools"
     "mdframed")
@@ -41,22 +42,15 @@
     "dt"
     "defeq"
     "del"
-    "Ob"
-    "Tr"
     "iso"
     "revimplies"
-    "GL"
     "degrees"
     "cbrt"
     "id"
     "epsm"
-    "fl"
     "vx"
     "vy"
     "vq"
-    "supp"
-    "li"
-    "Pois"
     "scrB"
     "scrF"
     "EE"
@@ -66,7 +60,6 @@
     "distto"
     "Gauss"
     "meet"
-    "D"
     "Im")
    (LaTeX-add-labels
     "#1")
