3549df5d6132b4fd6b6e8d2cba4cf0dd44d368d7
John Michael Wu <johnmichaelwu@gmail.com>
2020-01-29 18:43:06 +0800

    Remove everything added by Custom in init.el.

--- emacs/.emacs.d/init.el
+++ emacs/.emacs.d/init.el
@@ -12,20 +12,3 @@
 ;; load literate configuration
 (setq vc-follow-symlinks nil) ; ignore warning
 (org-babel-load-file "~/.emacs.d/config.org")
-
-
-(custom-set-variables
- ;; custom-set-variables was added by Custom.
- ;; If you edit it by hand, you could mess it up, so be careful.
- ;; Your init file should contain only one such instance.
- ;; If there is more than one, they won't work right.
- '(custom-enabled-themes (quote (manoj-dark)))
- '(package-selected-packages
-   (quote
-    (pyim emms lorem-ipsum bash-completion ace-window w3m google-this magit utop tuareg haskell-mode julia-mode julia-repl buffer-move python-mode sage-shell-mode auctex md4rd racket-mode markdown-mode proof-general))))
-(custom-set-faces
- ;; custom-set-faces was added by Custom.
- ;; If you edit it by hand, you could mess it up, so be careful.
- ;; Your init file should contain only one such instance.
- ;; If there is more than one, they won't work right.
- )
